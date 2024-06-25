3c3619381c27e252bbf7e12e2d89cbaaa42ee344
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-06-25 13:02:37 +0200

    portfolio-v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "./_composant/header"; 
-import Home from "./_page/home"
+import Header from "./_composant/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pouard Samuel - Portfolio",
-  description: "Je suis un développeur front-end React passionné, travaillant en freelance et basé à Lyon, âgé de 24 ans. Formé chez OpenClassrooms, j'ai acquis des compétences ...",
+  description:
+    "Je suis un développeur front-end React passionné, travaillant en freelance et basé à Lyon, âgé de 24 ans. Formé chez OpenClassrooms, j'ai acquis des compétences ...",
 };
 
 export default function RootLayout({
@@ -22,8 +22,7 @@ export default function RootLayout({
         <link rel="icon" href="../../public/asset/favicon-16x16.png" />
       </head>
       <body>
-        <Header /> 
-        {/* <Home /> */}
+        <Header />
         {children}
       </body>
     </html>
