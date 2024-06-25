74e9edaee46c7a811f2c2e254f369e7dda37b480
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-06-25 13:23:28 +0200

    portfolio-v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,25 +25,6 @@ import serviceLogoBack from "../../public/asset/external-backend-no-code-flatico
 
 
 
-
-const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  event.preventDefault();
-
-  const myForm = event.currentTarget;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData as any).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-
-
-
-
 export default function Home() {
   const techSrc = [
     react,
@@ -237,26 +218,111 @@ export default function Home() {
       <section>
 
 
-      <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+      <form
+                    name="contact"
+                    data-netlify="true"
+                    netlify-honeypot="bot-field"
+                    hidden
+                  >
+                    <input type="text" name="firstname" />
+                    <input type="text" name="lastname" />
+                    <input type="email" name="email" />
+                    <textarea name="message" />
+
+                    <input type="email" name="email" />
+                    <textarea name="message"></textarea>
+                  </form>
+                  <form
+                    className="p-8 text-base bg-white rounded-xl"
+                    name="contact"
+                    method="POST"
+                    netlify-honeypot="bot-field"
+                    action="#"
+                    data-netlify="true"
+                  >
+                    <div className="flex flex-wrap mb-6 -mx-3">
+                      <div>
+                        <input type="hidden" name="form-name" value="contact" />
+                      </div>
+                      <div className="hidden">
+                        <label>
+                          Don’t fill this out if you’re human:{" "}
+                          <input name="bot-field" />
+                        </label>
+                      </div>
+                      <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
+                        <label
+                          className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
+                          htmlFor="firstname"
+                        >
+                          First Name
+                        </label>
+                        <input
+                          className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border rounded appearance-none md:mb-3 focus:outline-none focus:bg-white focus:border-gray-300"
+                          id="firstname"
+                          type="text"
+                          placeholder="Jane"
+                          name="firstname"
+                        />
+                      </div>
+                      <div className="w-full px-3 md:w-1/2">
+                        <label
+                          className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
+                          htmlFor="lastname"
+                        >
+                          Last Name
+                        </label>
+                        <input
+                          className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-300"
+                          id="lastname"
+                          name="lastname"
+                          type="text"
+                          placeholder="Doe"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex flex-wrap mb-6 -mx-3">
+                      <div className="w-full px-3">
+                        <label
+                          className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
+                          htmlFor="email"
+                        >
+                          E-mail
+                        </label>
+                        <input
+                          className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-300"
+                          id="youremail"
+                          name="email"
+                          placeholder="janedoe@mail.com"
+                          type="email"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex flex-wrap mb-6 -mx-3">
+                      <div className="w-full px-3">
+                        <label
+                          className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
+                          htmlFor="message"
+                        >
+                          Message
+                        </label>
+                        <textarea
+                          className="block w-full h-48 px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none resize-none no-resize focus:outline-none focus:bg-white focus:border-gray-300"
+                          id="message"
+                          name="message"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className="flex flex-row justify-center">
+                      {/* TODO: Make the button look better and rounded like the landing */}
+                      <button
+                        type="submit"
+                        className="px-4 py-2 text-sm font-bold leading-5 text-center text-white transition duration-150 ease-in-out bg-orange-500 rounded-full shadow-lg focus:outline-none hover:opacity-75 focus:opacity-75"
+                      >
+                        Send Message
+                      </button>
+                    </div>
+                  </form>
 
 
       </section>
