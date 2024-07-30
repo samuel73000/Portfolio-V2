af54058dc36adac8987d7c01a449b7b8ba5984ad
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-07-30 17:23:11 +0200

    porrtfolio-v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,15 @@ export default function RootLayout({
 
 
  
+      
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-LSS5Y57MSS"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag()dataLayer.push(arguments);
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-LSS5Y57MSS');
 </script>
-
 
 
 
