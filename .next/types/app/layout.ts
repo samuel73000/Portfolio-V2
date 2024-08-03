f20a1855e980b4e92869c468b9eed6c52d0bde0f
samuel73000 <50187969+samuel73000@users.noreply.github.com>
2024-08-03 18:14:44 +0200

    portfolio-v2

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,8 +1,8 @@
 // File: C:\Users\sam\Desktop\DEV APP JAVASRIPT REACT\Portfolio-V2\src\app\layout.tsx
-import * as entry from '../../../src/app/layout.js'
+import * as entry from '../../../src/app/app.jsx'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../src/app/layout.js')
+type TEntry = typeof import('../../../src/app/app.jsx')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
